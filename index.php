be1530f9980d263d25364e683df6501b005d956e
mukhtar2944 <50269513+mukhtar2944@users.noreply.github.com>
2020-06-05 16:13:14 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php  
+        
+        $firstname = 'Mukhtar';
+        $lastname = 'Yusuf';
+        $id = 'HNG-05193';
+        $language = 'PHP';
+        $email = 'Mukhtarapril2000@gmail.com';
+        echo "Hello World, this is [".$firstname."] [".$lastname."] with HNGi7 ID [".$id."]  using [".$language."] for stage 2 task. [".$email."]";
+?>      
